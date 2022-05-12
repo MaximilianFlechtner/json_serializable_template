fc250fd5cafdd3c5654e10dc56b1b515a269c6a6
Maximilian Flechtner <66118057+MaximilianFlechtner@users.noreply.github.com>
2022-05-12 10:49:23 +0200

    first commit

---
+++ __brick__/{{name.snakeCase()}}.dart
@@ -0,0 +1,15 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part '{{name.snakeCase()}}.g.dart';
+
+@JsonSerializable()
+class {{name.pascalCase()}} {
+  
+  {{name.pascalCase()}}();
+
+
+  factory {{name.pascalCase()}}.fromJson(Map<String, dynamic> json) =>
+      _${{name.pascalCase()}}FromJson(json);
+
+  Map<String, dynamic> toJson() => _${{name.pascalCase()}}ToJson(this);
+}
